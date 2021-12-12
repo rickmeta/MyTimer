a02d2f4659a78691060aa8d8e1255957d6f82c8a
rickmeta <rickmeta45@gmail.com>
2021-12-13 08:45:39 +0900

    カウントダウン機能、タイマー設定機能、カウントダウン表示実装

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -8,10 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var timerHandler: Timer?
+    @State var count = 0
+    @AppStorage("timer_value") var timerValue = 10
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color("backgroundTimer")
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing:30.0) {
+                    Text("残り\(timerValue - count)秒")
+                        .font(.largeTitle)
+                    
+                    HStack {
+                        Button(action: {
+                            startTimer()
+                        }) {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                        
+                        Button(action: {
+                            if let unwrapedTimerHandler = timerHandler {
+                                if unwrapedTimerHandler.isValid == true {
+                                    unwrapedTimerHandler.invalidate()
+                                }
+                            }
+                        }) {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                    
+                }
+                .onAppear{
+                    count = 0
+                    
+                    if let unwrapedTimerHandler = timerHandler {
+                        if unwrapedTimerHandler.isValid == true {
+                            unwrapedTimerHandler.invalidate()
+                        }
+                    }
+                }
+                
+            }
+            .navigationBarItems(trailing:
+                                    NavigationLink(destination: SettingView()){
+                Text("秒設定")
+            }
+            )
+        }
     }
+    
+    func countDownTimer() {
+        count += 1
+        
+        if timerValue - count <= 0 {
+            timerHandler?.invalidate()
+        }
+    }
+    
+    func startTimer() {
+        if let unwrapedTimerHandler = timerHandler {
+            if unwrapedTimerHandler.isValid == true {
+                return
+            }
+        }
+        
+        if timerValue - count <= 0 {
+            count = 0
+        }
+        
+        timerHandler = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ _ in
+            countDownTimer()
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
