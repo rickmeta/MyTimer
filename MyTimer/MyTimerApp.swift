564b47edb9dcea5eadbe12537c1c44fa5a0f545c
rickmeta <rickmeta45@gmail.com>
2021-12-13 06:36:04 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by rickmeta on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
